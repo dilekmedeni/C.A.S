bef06a778c6292313529a15e2717cda6882cde15
BerkeIKU <62150369+Broqoli@users.noreply.github.com>
2020-03-27 16:23:30 +0300

    Main Page düzenlendi

--- prj/MainPage.aspx.designer.cs
+++ prj/MainPage.aspx.designer.cs
@@ -15,6 +15,24 @@ namespace Coiffeur_Appointment_System
     {
 
         /// <summary>
+        /// Label11 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label11;
+
+        /// <summary>
+        /// DataList1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DataList DataList1;
+
+        /// <summary>
         /// Label1 control.
         /// </summary>
         /// <remarks>
